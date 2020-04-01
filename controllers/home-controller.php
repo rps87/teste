6fbbc1b516d8809e3cc1ec4493498176e29d4c77
Raphael Santos <rps.indio@gmail.com>
2020-04-01 15:31:03 -0300

    quarta implementacao, ja funcionando no padrao com MVC com CSS

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -4,6 +4,9 @@
         public function index() {
             $this->title = 'Home';
             $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+            require ABSPATH . '/views/_includes/header.php';
+            require ABSPATH . '/views/_includes/menu.php';
             require ABSPATH . '/views/home/home-view.php';
+            require ABSPATH . '/views/_includes/footer.php';
         }
     }
--- views/login/login-view.php
+++ views/login/login-view.php
@@ -1,5 +1,23 @@
 <?php if ( ! defined('ABSPATH')) exit; ?>
+
 <div class="wrap">
-	<p>Olá, <br>
-	Você está em LOGIN, e o padrão MVC está funcionando.</p>
-</div>
+
+	<form method="post">
+		<table class="form-table">
+			<tr>
+				<td>Usuario</td> 
+				<td><input name="userdata[usuario]"></td>
+			</tr>
+			<tr>
+				<td>Senha </td>
+				<td><input type="password" name="userdata[usuario_password]"></td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" value="Logar"> 
+				</td>
+			</tr>
+		</table>
+	</form>
+	
+</div> <!-- .wrap -->
